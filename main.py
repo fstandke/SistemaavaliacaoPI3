d427e62d00e416559a9ad7a15dc26a852060205e
fstandke <49998936+fstandke@users.noreply.github.com>
2024-11-13 19:59:35 +0000

    incluindo sobre.html

--- main.py
+++ main.py
@@ -45,6 +45,9 @@ class AvaliacaoForm(FlaskForm):
 def index():
     return render_template('index.html')
 
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
 
 # Cadastro do Professor
 @app.route('/cadastro/')
