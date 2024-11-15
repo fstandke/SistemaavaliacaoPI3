a956f5aebf4abaf4fb147e1c38479517b45ace79
fstandke <49998936+fstandke@users.noreply.github.com>
2024-11-15 19:04:18 +0000

    pop up java script confirmando cadastro avaliacao

---
+++ static/script.js
@@ -0,0 +1,9 @@
+function enviar_avaliacao() {
+  alert("Avaliação cadastrada com sucesso!")
+}
+
+let botao=document.querySelector("#botao")
+
+botao.addEventListener("click", () =>{
+  enviar_avaliacao()
+})
